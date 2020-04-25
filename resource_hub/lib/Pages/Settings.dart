4106408391a59eed2f2109ec432390e822f93acb
vinith reddy <vinithreddynarra@gmail.com>
2020-04-25 18:12:59 +0530

    add settings design

--- resource_hub/lib/Pages/Settings.dart
+++ resource_hub/lib/Pages/Settings.dart
@@ -6,17 +6,117 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  var icon_color= Colors.blue.shade200;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: Icon(Icons.arrow_back),
         title: Text("Settings"),
+        actions: <Widget>[
+          Padding(
+              padding: EdgeInsetsDirectional.only(end: 16),
+              child: Icon(Icons.brightness_2)
+          )
+
+        ],
       ),
-      body: Center(
-        child: Container(
-          child: Text("Settings"),
-        ),
-      ),
+        body: Container(
+          child: ListView(
+            padding: EdgeInsets.fromLTRB(8, 8, 8, 0),
+            children: <Widget>[
+              GestureDetector(
+                behavior: HitTestBehavior.translucent,
+                onTap: () {
+                  debugPrint("acc pressed");
+                },
+                child: ListTile(
+                  leading:  Icon(
+                      Icons.build,
+                      color: icon_color
+                  ),
+                  title: Text("Account"),
+                  subtitle: Text("change account, privacy"),
+                ),
+              ),
+
+              GestureDetector(
+                behavior: HitTestBehavior.translucent,
+                onTap: () {
+                  debugPrint("acc pressed");
+                },
+                child: ListTile(
+                  leading: Icon(
+                      Icons.exit_to_app,
+                      color: icon_color
+                  ),
+                  title: Text("Logout"),
+                  subtitle: Text("change account, privacy"),
+                ),
+              ),
+
+              GestureDetector(
+                behavior: HitTestBehavior.translucent,
+                onTap: () {
+                  debugPrint("acc pressed");
+                },
+                child: ListTile(
+                  leading: Icon(
+                      Icons.notifications,
+                      color: icon_color
+                  ),
+                  title: Text("Notifications"),
+                  subtitle: Text("turn on and off app notifications"),
+                ),
+              ),
+
+              GestureDetector(
+                behavior: HitTestBehavior.translucent,
+                onTap: () {
+                  debugPrint("acc pressed");
+                },
+                child: ListTile(
+                  leading: Icon(
+                      Icons.bug_report,
+                      color: icon_color
+                  ),
+                  title: Text("Bug Report"),
+                  subtitle: Text("change account, privacy"),
+                ),
+              ),
+
+              GestureDetector(
+                behavior: HitTestBehavior.translucent,
+                onTap: () {
+                  debugPrint("acc pressed");
+                },
+                child: ListTile(
+                  leading: Icon(
+                    Icons.help,
+                    color: icon_color,
+                  ),
+                  title: Text("Help"),
+                  subtitle: Text("FAQ,contact us,privacy policy"),
+                ),
+              ),
+
+              GestureDetector(
+                behavior: HitTestBehavior.translucent,
+                onTap: () {
+                  debugPrint("acc pressed");
+                },
+                child: ListTile(
+                    leading: Icon(
+                      Icons.people,
+                      color: icon_color,
+                    ),
+                    title: Text("Invite a friend")
+                ),
+              ),
+
+            ],
+          ),
+        )
     );
   }
 }
